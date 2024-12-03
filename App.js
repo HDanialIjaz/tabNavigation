a1b51ed9c654f05111ef86d83b7e25c24ee9060d
Hafiz Muhammad Danial <danialijaz098@gmail.com>
2024-12-03 22:47:20 +0500

    refactor
    add tabBAr navigation in code

--- App.js
+++ App.js
@@ -1,27 +1,41 @@
 import React from "react";
-import { View,Text,StyleSheet } from "react-native";
+import {View, Text} from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Icon from "react-native-vector-icons/Ionicons";
+import Icon  from "react-native-vector-icons/Ionicons";
+import Home from "./home_screen.js"
+import Notification from "./notification_screen.js"
+import Setting from "./setting_screen.js"
 
+const Tab = createBottomTabNavigator();
 
-const notification=()=>{
-  return(
-  <View style={style.container}>
-    <Text style={style.text}>Notification</Text>
-    </View>
-  );
+const App =()=>(
+  <NavigationContainer>
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+     tabBarIcon:({ focused,color,size }) => {
+      let iconName;
+      if(route.name === "Home"){
+        iconName = focused ? "home" : "home-outline"
+      }
+      if( route.name === "Notification"){
+        iconName = focused ? "notificatioins" : "notifications-outline"
+      }
+      if(route.name === "Setting"){
+        iconName = focused ? "settings" : "settings-outline"
+      }
+      return <Icon name={iconName} color={color} size={size} />
+     }, 
+      tabBarActiveTintColor : "blue",
+      tabBarInactiveTintColor : "gray",
+    })}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Notification" component={Notification} />
+      <Tab.Screen name="Setting" component={Setting} />
+    </Tab.Navigator>
+  </NavigationContainer>
+    
+);
 
-}
-export default notification;
-
-const style=StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center'
-  },
-  text:{
-    fontSize:25
-  }
-});
+export default App;
